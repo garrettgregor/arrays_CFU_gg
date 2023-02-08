ee84252587c7e5e9671f65ea93687420bc894d4b
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-08 10:54:42 -0700

    Call and comment each array

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ friends = ["Chris", "Angie", "Philip", "Michele"]
 numbers = [1, 5, 25, 125]
 decimals = [1.1, 2.2, 4.4, 8.8]
 yes_no = [false, true, true, false]
+
+# This will print the last element of the friends array - "Michele"
+puts friends[3]
+
+# This is print the length of the numbers array - 4
+puts numbers.length
+
+# This will remove the last elements of the decimals array - 8.8
+decimals.pop
+
+#This will add another element to the yes_no array - false
+yes_no.unshift(false)
